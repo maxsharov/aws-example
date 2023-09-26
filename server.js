dde043f801f8035b3a05509413e461cd4aa5ed81
Maxguitarman <max.sharov@gmail.com>
2023-09-26 19:32:02 +0300

    Add products endpoint

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ app.get('/', (req, res) => {
     res.send('Node.js on AWS')
 })
 
+app.get('/products', (req, res) => {
+    res.send([
+        {
+            productId: '101',
+            price: 100
+        },
+        {
+            productId: '102',
+            price: 150
+        }
+    ])
+})
+
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
 })
